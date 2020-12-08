223e1c57b37286e124ccee0e7edf92dce8c8b89c
Linda Perez <lindaperez@Lindas-MacBook-Pro-2.local>
2020-12-08 15:52:49 -0800

    starting project again

---
+++ app/models.py
@@ -0,0 +1,189 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class CatStateFeature(models.Model):
+    title = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'cat_state_feature'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    started = models.DateTimeField()
+    ended = models.DateTimeField(blank=True, null=True)
+    backend = models.CharField(max_length=50)
+    frontend = models.CharField(max_length=50)
+    repo_name = models.CharField(max_length=100)
+    repo_url = models.CharField(max_length=500)
+
+    class Meta:
+        managed = False
+        db_table = 'project'
+
+
+class RelProjectComment(models.Model):
+    id_project = models.ForeignKey(Project, models.DO_NOTHING, db_column='id_project')
+    comment = models.CharField(max_length=500)
+
+    class Meta:
+        managed = False
+        db_table = 'rel_project_comment'
+
+
+class RelProjectFeature(models.Model):
+    feat_state = models.ForeignKey(CatStateFeature, models.DO_NOTHING, db_column='feat_state')
+    description = models.CharField(max_length=500)
+    id_project = models.ForeignKey(Project, models.DO_NOTHING, db_column='id_project')
+
+    class Meta:
+        managed = False
+        db_table = 'rel_project_feature'
+
+
+class RelProjectUser(models.Model):
+    id_project = models.ForeignKey(Project, models.DO_NOTHING, db_column='id_project')
+    id_user = models.ForeignKey('User', models.DO_NOTHING, db_column='id_user')
+
+    class Meta:
+        managed = False
+        db_table = 'rel_project_user'
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
+
+
+class View(models.Model):
+    id_project = models.ForeignKey(Project, models.DO_NOTHING, db_column='id_project')
+    description = models.CharField(max_length=50)
+    picpath = models.CharField(max_length=500)
+
+    class Meta:
+        managed = False
+        db_table = 'view'
